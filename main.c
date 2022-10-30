f4e2dc56481c655e10fc2cb19049e2ce4322af5c
Luana <lupinheiro.77@hotmail.com>
2022-10-30 17:47:27 -0300

    Modularização inicial

--- main.c
+++ main.c
@@ -9,79 +9,97 @@
 #define coluna 20
 #define linha 10
 
+void desenhaMapaNoEscuro(int pi, int pj, int ai, int aj)
+{
+    for(int i=0; i<linha; i++)
+    {
+        for(int j=0; j<coluna; j++)
+        {
+            if(i==pi && j==pj)
+            {
+                printf(" P");
+            }
+            else if(i>0 && j>0 && i<linha-1 && j<coluna-1)
+            {
+                if( i==pi+1 && j==pj || i==pi+1 && j==pj+1 || i==pi+1 && j==pj-1 ||
+                        i==pi-1 && j==pj   || i==pi-1 && j==pj+1 || i==pi-1 && j==pj-1 ||
+                        i==pi   && j==pj+1 || i==pi && j==pj-1)
+                {
+                    if (i == ai && j == aj)
+                    {
+                        printf(" ,");
+                    }
+                    else
+                    {
+                        printf(" .");
+                    }
+                }
+                else
+                {
+                    printf("  ");
+                }
+            }
+            else if(i==0 && pi==1 && j==pj || i==0 && pi==1 && j==pj-1 || i==0 && pi==1 && j==pj+1 ||
+                    j==0 && pj==1 && i==pi || j==0 && pj==1 && i==pi-1 || j==0 && pj==1 && i==pi+1 ||
+                    j==coluna-1 && pj==coluna-2 && i==pi || j==coluna-1 && pj==coluna-2 && i==pi-1 || j==coluna-1 && pj==coluna-2 && i==pi+1 ||
+                    i==linha-1 && pi==linha-2 && j==pj || i==linha-1 && pi==linha-2 && j==pj-1 || i==linha-1 && pi==linha-2 && j==pj+1)
+            {
+                printf(" #");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void desenhaMapaComLuz(int pi, int pj)
+{
+    for (int i = 0; i < linha; i++)
+    {
+        for (int j = 0; j < coluna; j++)
+        {
+            if (i == 0 || j == 0 || j == coluna - 1 || i == linha - 1)
+                printf(" #");
+            else if (j == pj && i == pi)
+                printf(" P");
+            else
+                printf(" .");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+}
+
+
+void desenhaMapa(int pi, int pj)
+{
+    int ai = 5, aj = 7;
+    if(pi != ai && pj != aj || pi == ai && pj !=aj || pi != ai && pj ==aj )
+        desenhaMapaNoEscuro(pi, pj, ai, aj);
+    else
+        desenhaMapaComLuz(pi, pj);
+}
+
 void main()
 {
     int pi=1, pj=1;
     char comando;
     int movimentoRealizado;
 
-    int ai = 5, aj = 7;
+
 
     for( ; ; )
     {
         system("CLS");
 
-        if(pi != ai && pj != aj || pi == ai && pj !=aj || pi != ai && pj ==aj )
-        {
+        desenhaMapa(pi, pj);
 
-            for(int i=0; i<linha; i++)
-            {
-                for(int j=0; j<coluna; j++)
-                {
-                    if(i==pi && j==pj)
-                    {
-                        printf(" P");
-                    }
-                    else if(i>0 && j>0 && i<linha-1 && j<coluna-1)
-                    {
-                        if( i==pi+1 && j==pj || i==pi+1 && j==pj+1 || i==pi+1 && j==pj-1 ||
-                            i==pi-1 && j==pj   || i==pi-1 && j==pj+1 || i==pi-1 && j==pj-1 ||
-                            i==pi   && j==pj+1 || i==pi && j==pj-1)
-                        {
-                            if (i == ai && j == aj){
-                                printf(" ,");
-                            } else {
-                            printf(" .");
-                            }
-                        }
-                        else
-                        {
-                            printf("  ");
-                        }
-                    }
-                    else if(i==0 && pi==1 && j==pj || i==0 && pi==1 && j==pj-1 || i==0 && pi==1 && j==pj+1 ||
-                            j==0 && pj==1 && i==pi || j==0 && pj==1 && i==pi-1 || j==0 && pj==1 && i==pi+1 ||
-                            j==coluna-1 && pj==coluna-2 && i==pi || j==coluna-1 && pj==coluna-2 && i==pi-1 || j==coluna-1 && pj==coluna-2 && i==pi+1 ||
-                            i==linha-1 && pi==linha-2 && j==pj || i==linha-1 && pi==linha-2 && j==pj-1 || i==linha-1 && pi==linha-2 && j==pj+1)
-                    {
-                        printf(" #");
-                    }
-                    else
-                    {
-                        printf("  ");
-                    }
-                }
-                printf("\n");
-            }
 
-        }
-        else
-        {
-            for (int i = 0; i < linha; i++)
-            {
-                for (int j = 0; j < coluna; j++)
-                {
-                    if (i == 0 || j == 0 || j == coluna - 1 || i == linha - 1)
-                        printf(" #");
-                    else if (j == pj && i == pi)
-                        printf(" P");
-                    else
-                        printf(" .");
-                }
-                printf("\n");
-            }
-            printf("\n");
-        }
+
         printf("Comando: w (cima)\td (direita)\ts (baixo)\ta (esquerda)\n");
         printf("Digite um comando: ");
         comando = getch();
